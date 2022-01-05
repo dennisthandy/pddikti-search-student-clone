c7037d4e2a06149d4dda54887cbeb2280232d961
dennisthandy <dennisthandy15@gmail.com>
2022-01-05 20:33:01 +0700

    change next-config

--- next.config.js
+++ next.config.js
@@ -16,29 +16,26 @@ const nextConfig = {
   images: {
     domains: ["avatars.dicebear.com"],
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       // matching all API routes
-  //       source: "/api/:path*",
-  //       headers: [
-  //         { key: "Access-Control-Allow-Credentials", value: "true" },
-  //         { key: "Access-Control-Allow-Origin", value: "*" },
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Headers",
-  //           value:
-  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-  //         },
-  //         { key: "Connection", value: "keep-alive" },
-  //         { key: "Content-Type", value: "application/json; charset=utf-8"}
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-CSRF-Token, Content-Type",
+          },
+          { key: "Connection", value: "keep-alive" },
+          { key: "Content-Type", value: "application/json; charset=utf-8" },
+        ],
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
